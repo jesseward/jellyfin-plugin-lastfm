14705953f3cbd1a09ccf845927c82d6cbd35fa08
Luke Pulverenti <luke.pulverenti@gmail.com>
2018-06-20 12:47:23 -0400

    initial

---
+++ Lastfm/Configuration/PluginConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediaBrowser.Model.Plugins;
+
+namespace Lastfm.Configuration
+{
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+    }
+}
