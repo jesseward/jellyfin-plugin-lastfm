1be262c667d03ced728337632497005dcf5b8a42
Luke Pulverenti <luke.pulverenti@gmail.com>
2018-09-30 17:20:49 -0400

    1.0.6

--- Lastfm/Plugin.cs
+++ Lastfm/Plugin.cs
@@ -16,7 +16,7 @@
     /// <summary>
     /// Class Plugin
     /// </summary>
-    public class Plugin : BasePlugin<PluginConfiguration>
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasThumbImage
     {
         /// <summary>
         /// Flag set when an Import Syncing task is running
