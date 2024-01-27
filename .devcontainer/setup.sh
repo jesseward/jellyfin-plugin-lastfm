c30415ac6bf07c5e1c517bd4c28a68c04095dffb
Jesse Ward <jesse.ward@gmail.com>
2024-01-27 22:47:31 +0000

    Use the jellyfin devcontainer instead of docker-in-docker

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
 
-JELLYFIN_VOLUME="/workspaces/jf"
+JELLYFIN_VOLUME="/opt/jellyfin"
 PROJECT_HOME="/workspaces/jellyfin-plugin-lastfm/"
 
 cd ${PROJECT_HOME} && dotnet restore
 
-mkdir -p ${JELLYFIN_VOLUME}/{config,cache,media}
-mkdir -p ${JELLYFIN_VOLUME}/config/plugins
-
-cp -R ${PROJECT_HOME}/tests/* ${JELLYFIN_VOLUME}/media/
+sudo mkdir -p ${JELLYFIN_VOLUME}/config/plugins
+sudo mkdir -p ${JELLYFIN_VOLUME}/media
+sudo cp -R ${PROJECT_HOME}/tests/* ${JELLYFIN_VOLUME}/media/
