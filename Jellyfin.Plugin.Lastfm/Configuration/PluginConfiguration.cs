168a7cf5377121b95cf17cbeae3c7e98900e9478
Jesse Ward <jesse.ward@pointclickcare.com>
2024-05-16 12:47:54 -0400

    Linting across Jellyfin.Plugin.Lastfm/Configuration

--- Jellyfin.Plugin.Lastfm/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.Lastfm/Configuration/PluginConfiguration.cs
@@ -1,21 +1,25 @@
-﻿namespace Jellyfin.Plugin.Lastfm.Configuration
+﻿using System;
+using System.Collections.Generic;
+using Jellyfin.Plugin.Lastfm.Models;
+using MediaBrowser.Model.Plugins;
+
+namespace Jellyfin.Plugin.Lastfm.Configuration;
+
+/// <summary>
+/// Plugin configuration class for LastFM plugin.
+/// </summary>
+public class PluginConfiguration : BasePluginConfiguration
 {
-    using Models;
-    using MediaBrowser.Model.Plugins;
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PluginConfiguration" /> class.
+    /// </summary>
+    public PluginConfiguration()
+    {
+        LastfmUsers = Array.Empty<LastfmUser>();
+    }
 
     /// <summary>
-    /// Class PluginConfiguration
+    /// Gets or sets all configured LastfmUsers.
     /// </summary>
-    public class PluginConfiguration : BasePluginConfiguration
-    {
-        public LastfmUser[] LastfmUsers { get; set; }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PluginConfiguration" /> class.
-        /// </summary>
-        public PluginConfiguration()
-        {
-            LastfmUsers = new LastfmUser[] { };
-        }
-    }
+    public IEnumerable<LastfmUser> LastfmUsers { get; set; }
 }
